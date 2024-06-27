a93ebd98208327c459367df575d9f67b60b18711
Kochev Ilya <kochev0987@gmail.com>
2024-06-27 20:23:26 +0300

    Passed 12.3

--- PythonForPro/12.3/8.py
+++ PythonForPro/12.3/8.py
@@ -1,5 +1,11 @@
-import random 
+import random
 
+s = 4
 n = 10**6
-s = 1
+k = 0
 for i in range(n):
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if x ** 2 + y ** 2 <= 1:
+        k += 1
+print(float(k) / float(n) * s)
